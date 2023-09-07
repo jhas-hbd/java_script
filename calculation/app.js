7d733e2ac03de52e8c439231561053b5fa092f2f
EC2 Default User <ec2-user@ip-172-31-8-148.ap-northeast-1.compute.internal>
2023-09-07 23:44:47 +0000

    Add alert

--- calculation/app.js
+++ calculation/app.js
@@ -4,4 +4,22 @@ let int2 = -10;
 
 let float1 = 3.14;
 
-let str1 = 'JavaScriptを覚えよう';
+// let str1 = 'JavaScriptを覚えよう';
+
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
+
+alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
